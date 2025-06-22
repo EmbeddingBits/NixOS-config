70606bf83ab5b75ac187fe1ecdf6a29e2e68bd7e
EmbeddingBits <tspamiitesh@gmail.com>
2025-06-22 13:04:24 +0530

    Final

--- home.nix
+++ home.nix
@@ -52,17 +52,17 @@
 
       home.file = {
 
-            #  ".config/hypr/hyprland.conf".source = ./dotfiles/hypr/hyprland.conf;
-            #  ".config/fish/config.fish".source = ./dotfiles/fish/config.fish;
-            #  ".config/hypr/hyprlock.conf".source = ./dotfiles/hypr/hyprlock.conf;
-            #  ".config/waybar/config.jsonc".source = ./dotfiles/waybar/config.jsonc;
-            #  ".config/waybar/style.css".source = ./dotfiles/waybar/style.css;
+  ".config/hypr/hyprland.conf".source = ./dotfiles/hypr/hyprland.conf;
+  ".config/fish/config.fish".source = ./dotfiles/fish/config.fish;
+  ".config/hypr/hyprlock.conf".source = ./dotfiles/hypr/hyprlock.conf;
+  ".config/waybar/config.jsonc".source = ./dotfiles/waybar/config.jsonc;
+  ".config/waybar/style.css".source = ./dotfiles/waybar/style.css;
   ".config/rofi/config.rasi".source = ./dotfiles/rofi/config.rasi;
   ".config/rofi/fonts.rasi".source = ./dotfiles/rofi/fonts.rasi;
   ".config/rofi/powermenu.rasi".source = ./dotfiles/rofi/powermenu.rasi;
   ".config/rofi/nord.rasi".source = ./dotfiles/rofi/nord.rasi;
-            #  ".config/kitty/kitty.conf".source = ./dotfiles/kitty/kitty.conf;
-            #  ".config/kitty/current-theme.conf".source = ./dotfiles/kitty/current-theme.conf;
+  ".config/kitty/kitty.conf".source = ./dotfiles/kitty/kitty.conf;
+  ".config/kitty/current-theme.conf".source = ./dotfiles/kitty/current-theme.conf;
   ".config/cava/config".source = ./dotfiles/cava/config;
   };
 }
